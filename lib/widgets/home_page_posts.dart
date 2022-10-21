e198d58a14507057959836d5fa629c67e17259ea
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2022-10-21 08:50:58 -0300

    Atualização

--- lib/widgets/home_page_posts.dart
+++ lib/widgets/home_page_posts.dart
@@ -32,16 +32,17 @@ class HomePagePosts extends StatelessWidget {
             ),
           ),
           const SizedBox(
-             height: 15,
+            height: 15,
           ),
           GridView.count(
-            physics: NeverScrollableScrollPhysics(),
+            physics: const NeverScrollableScrollPhysics(),
             crossAxisCount: 2,
             childAspectRatio: 0.7,
             crossAxisSpacing: 15,
             mainAxisSpacing: 15,
             shrinkWrap: true,
             children: [
+              for(int i=1; i<9; i++)
               Container(
                 padding: const EdgeInsets.all(10),
                 decoration: BoxDecoration(
@@ -53,6 +54,69 @@ class HomePagePosts extends StatelessWidget {
                       spreadRadius: 1,
                       blurRadius: 5,
                     ),
+                  ],
+                ),
+                child: Column(
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        //Navigator.pushNamed(context, 'itemPage');
+                      },
+                      child: Image.asset(
+                        'images/$i.png',
+                        fit: BoxFit.contain,
+                        height: 120,
+                        width: 110,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 13,
+                    ),
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            'Nome do Item',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 12,
+                          ),
+                          Row(
+                            children: const [
+                              Text(
+                                'R\$50',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xFFFFB608),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              Text(
+                                ' / 1K',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Spacer(),
+                              Icon(
+                                Icons.favorite_border,
+                                color: Color(0xFFFFB608),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    )
                   ],
                 ),
               )
