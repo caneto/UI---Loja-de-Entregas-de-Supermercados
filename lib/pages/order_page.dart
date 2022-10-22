2337c5e24b231edf363af38c76726d8764bbe48e
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2022-10-22 12:41:40 -0300

    Atualização

--- lib/pages/order_page.dart
+++ lib/pages/order_page.dart
@@ -1,30 +1,186 @@
 import 'package:flutter/material.dart';
 
 class OrderPage extends StatelessWidget {
+  const OrderPage({Key? key}) : super(key: key);
 
-  const OrderPage({ Key? key }) : super(key: key);
-
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           body: ListView(
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          Container(
+            alignment: Alignment.centerLeft,
+            margin: const EdgeInsets.all(20),
+            child: InkWell(
+              onTap: (() {
+                Navigator.pop(context);
+              }),
+              child: const Icon(
+                Icons.arrow_back,
+                size: 28,
+              ),
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Container(
+                margin: const EdgeInsets.only(left: 15, top: 20),
                 alignment: Alignment.centerLeft,
-                margin: const EdgeInsets.all(20),
-                child: InkWell(
-                  onTap: (() {
-                     Navigator.pop(context);
-                    }
+                child: const Text(
+                  'Detalhes da Ordem Final',
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Color.fromARGB(162, 0, 0, 0),
+                    fontWeight: FontWeight.bold,
                   ),
-                  child: Icon(
-                    Icons.arrow_back,
-                    size: 28,
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(left: 15, top: 20),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                width: 370,
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Primeiro Nome',
+                    hintStyle: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(left: 15, top: 20),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                width: 370,
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Ultimo Nome',
+                    hintStyle: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(left: 15, top: 20),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                width: 370,
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Celular',
+                    hintStyle: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(left: 15, top: 20),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                width: 370,
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Email',
+                    hintStyle: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(left: 15, top: 20),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                width: 370,
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Endereço',
+                    hintStyle: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(left: 15, top: 20),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                width: 370,
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Cidade',
+                    hintStyle: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(left: 15, top: 20),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                width: 370,
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'CEP',
+                    hintStyle: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 50),
+              InkWell(
+                onTap: (() {}),
+                child: Container(
+                  alignment: Alignment.center,
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                  margin: const EdgeInsets.symmetric(horizontal: 20),
+                  decoration: BoxDecoration(
+                    color: const Color(0xFFFFB608),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: const Text(
+                    'Ordem Agora',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
               )
             ],
-           ),
-       );
+          )
+        ],
+      ),
+    );
   }
 }
